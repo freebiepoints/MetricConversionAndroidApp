52f300d7c7a34cffa4520b959680972a000f8577
JT Russell <jt.russell@ou.edu>
2026-02-10 13:37:29 -0600

    Added Readme.md and code comments

--- app/src/main/java/com/example/metricconversion/MainActivity.kt
+++ app/src/main/java/com/example/metricconversion/MainActivity.kt
@@ -7,8 +7,12 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-
+/**
+ * The main activity for the metric conversion application.
+ * This activity allows users to convert values between different units.
+ */
 class MainActivity : AppCompatActivity() {
+    // UI elements
     lateinit var etVal: EditText
     lateinit var btnMiKm: Button
     lateinit var btnKmMi: Button
@@ -18,11 +22,17 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnCK: Button
     lateinit var tvResult: TextView
 
+    /**
+     * Called when the activity is first created.
+     * This is where you should do all of your normal static set up:
+     * create views, bind data to lists, etc.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // Initialize UI elements
         etVal = findViewById(R.id.et_val)
         btnMiKm = findViewById(R.id.btn_mi_to_km)
         btnKmMi = findViewById(R.id.btn_km_to_mi)
@@ -30,21 +40,23 @@ class MainActivity : AppCompatActivity() {
         btnCF = findViewById(R.id.btn_c_to_f)
         btnFK = findViewById(R.id.btn_f_to_k)
         btnCK = findViewById(R.id.btn_c_to_k)
-
         tvResult = findViewById(R.id.tv_result)
 
+        // Set click listener for miles to kilometers conversion
         btnMiKm.setOnClickListener {
             val value = etVal.text.toString().toDouble()
             val result = value * 1.60934
             tvResult.text = "$value miles is $result km"
         }
 
+        // Set click listener for kilometers to miles conversion
         btnKmMi.setOnClickListener {
             val value = etVal.text.toString().toDouble()
             val result = value / 1.60934
             tvResult.text = "$value km is $result miles"
         }
 
+        // Set click listener for Fahrenheit to Celsius conversion
         btnFC.setOnClickListener {
             val value = etVal.text.toString().toDouble()
             val result = (value-32)*(0.55556)
@@ -52,6 +64,7 @@ class MainActivity : AppCompatActivity() {
             tvResult.text = "$value degrees Fahrenheit is $formattedresult degrees Celsius"
         }
 
+        // Set click listener for Celsius to Fahrenheit conversion
         btnCF.setOnClickListener {
             val value = etVal.text.toString().toDouble()
             val result = ((value*1.8) + 32)
@@ -59,6 +72,7 @@ class MainActivity : AppCompatActivity() {
             tvResult.text = "$value degrees Celsius is $formattedresult degrees Fahrenheit"
         }
 
+        // Set click listener for Fahrenheit to Kelvin conversion
         btnFK.setOnClickListener {
             val value = etVal.text.toString().toDouble()
             val result = ((value-32)*(0.55556)) + 273.15
@@ -66,6 +80,7 @@ class MainActivity : AppCompatActivity() {
             tvResult.text = "$value degrees Fahrenheit is $formattedresult degrees Kelvin"
         }
 
+        // Set click listener for Celsius to Kelvin conversion
         btnCK.setOnClickListener {
             val value = etVal.text.toString().toDouble()
             val result = value + 273.15
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Metric Conversion
+
+A simple Android application for converting between different units of measurement.
+
+## Features
+
+This app allows you to convert:
+
+*   **Distance:**
+    *   Miles to Kilometers
+    *   Kilometers to Miles
+*   **Temperature:**
+    *   Fahrenheit to Celsius
+    *   Celsius to Fahrenheit
+    *   Fahrenheit to Kelvin
+    *   Celsius to Kelvin
+
+## How to Use
+
+1.  Enter the numerical value you want to convert in the text field.
+2.  Click the button corresponding to the conversion you want to perform.
+3.  The result will be displayed on the screen.
